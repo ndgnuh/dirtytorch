f81b633de80acdeac05fe45adfd1cbf4d9ec451d
Hung Nguyen <hungnd@hiworld.com.vn>
2022-11-23 09:26:24 +0700

    add gpu, camera, cache folders

--- enter-dev.sh
+++ enter-dev.sh
@@ -1,14 +1,25 @@
 #!/bin/sh
-docker run -it \
-	--name "torch-dev-env" \
-    --volume /tmp/.X11-unix:/tmp/.X11-unix \
-    --volume $HOME/.Xauthority:/home/dev/.Xauthority \
-    --volume $PWD/:/home/dev/working \
-    --volume /dev:/dev \
-    --volume /sys:/sys \
-    --volume /proc:/proc \
-    --env DISPLAY=$DISPLAY \
-    --network host \
-    --entrypoint bash \
-    "ndgnuh/torch-dev-env" 
-
+# name="torch-dev-env"
+# count_container=$(docker ps --filter name="$name" -a | wc -l)
+# if [ $count_container -le 1 ]; then
+		# --name "$name" \
+	docker run -it \
+		--rm \
+		--volume /tmp/.X11-unix:/tmp/.X11-unix \
+		--volume $HOME/.Xauthority:/home/dev/.Xauthority \
+		--volume $PWD/:/home/dev/working \
+		--volume $HOME/.cache:/home/dev/.cache \
+		--volume $HOME/.deepface:/home/dev/.deepface \ # Deep face cache
+		--device /dev/video0:/dev/video0 \
+		--gpus all \
+		--env DISPLAY=$DISPLAY \
+		--network host \
+		--entrypoint bash \
+		"ndgnuh/torch-dev-env" 
+		# --volume /dev:/dev \
+		# --volume /sys:/sys \
+		# --volume /proc:/proc \
+	#
+# else
+# 	docker start -it $name bash
+# fi
