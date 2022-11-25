2ce967e74c0ebcb911c107221e334e4c136a46db
Hung Nguyen <hungnd@hiworld.com.vn>
2022-11-25 18:00:01 +0700

    new enter dev script

--- enter-dev.sh
+++ enter-dev.sh
@@ -1,25 +1,39 @@
-#!/bin/sh
-# name="torch-dev-env"
-# count_container=$(docker ps --filter name="$name" -a | wc -l)
-# if [ $count_container -le 1 ]; then
-		# --name "$name" \
-	docker run -it \
-		--rm \
-		--volume /tmp/.X11-unix:/tmp/.X11-unix \
-		--volume $HOME/.Xauthority:/home/dev/.Xauthority \
-		--volume $PWD/:/home/dev/working \
-		--volume $HOME/.cache:/home/dev/.cache \
-		--volume $HOME/.deepface:/home/dev/.deepface \ # Deep face cache
-		--device /dev/video0:/dev/video0 \
-		--gpus all \
-		--env DISPLAY=$DISPLAY \
-		--network host \
-		--entrypoint bash \
-		"ndgnuh/torch-dev-env" 
-		# --volume /dev:/dev \
-		# --volume /sys:/sys \
-		# --volume /proc:/proc \
-	#
-# else
-# 	docker start -it $name bash
-# fi
+#!/bin/bash
+args=""
+if [ -d $HOME/.deepface ]; then
+	args="$args --mount type=bind,source=$HOME/.deepface/,target=/home/dev/.deepface"
+fi
+if [ -d $HOME/.cache ]; then
+	args="$args --mount type=bind,source=$HOME/.cache/,target=/home/dev/.cache"
+fi
+if [ -e /dev/video0 ]; then
+	args="$args --device /dev/video0"
+fi
+if [ -f $HOME/.Xauthority ]; then
+	args=$args' --volume '"$HOME/.Xauthority:/home/dev/.Xauthority"
+fi
+echo "$args"
+docker run -it --rm $@ \
+	--mount type=bind,source=/dev,target=/dev \
+	--mount type=bind,source=/sys,target=/sys \
+	--mount type=bind,source=/proc,target=/proc \
+	--mount type=bind,source=/tmp/,target=/tmp \
+	--mount type=bind,source=$PWD,target=/home/dev/working \
+	--network host \
+	--user $(id -u):$(id -g) \
+	--gpus all \
+	--env DISPLAY=$DISPLAY \
+	$args \
+	"ndgnuh/torch-dev-env" 
+
+	# --mount type=bind,source=$HOME/.deepface,target=/home/dev/.deepface \
+	# --volume "/tmp/.X11-unix:/tmp/.X11-unix" \
+	# --volume "$HOME/.Xauthority:/home/dev/.Xauthority" \
+	# --volume "$PWD/:/home/dev/working" \
+	# --volume "$HOME/.cache:/home/dev/.cache" \
+	# --volume "$HOME/.deepface:/home/dev/.deepface" \ 
+# --cpus "all" \
+# --gpus "all" \
+	# --env DISPLAY=$DISPLAY \
+	# --network host \
+	# --entrypoint bash \
