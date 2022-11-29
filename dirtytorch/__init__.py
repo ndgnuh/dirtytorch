9daea74ee2795c982455fb77444b7c3d8d5b401a
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-11-29 22:23:53 +0700

    lazy deps

--- dirtytorch/__init__.py
+++ dirtytorch/__init__.py
@@ -1,7 +1,5 @@
-from pytorch_lightning import loggers
-
-
 def init_mlflow_logger(*args, tracking_uri=None, **kwargs):
+    from pytorch_lightning import loggers
     import mlflow
     logger = loggers.MLFlowLogger(*args, **kwargs, tracking_uri=tracking_uri)
     mlflow.set_tracking_uri(tracking_uri)
