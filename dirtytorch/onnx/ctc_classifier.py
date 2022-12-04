5feaf29f296d2e517c9fb75d25570f96c026f3af
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-12-04 13:28:43 +0700

    new snippet: onnx-ctc-classifier

---
+++ dirtytorch/onnx/ctc_classifier.py
@@ -0,0 +1,72 @@
+from typing import Optional, Callable
+from itertools import groupby
+from onnxruntime import InferenceSession, get_available_providers
+from PIL import Image
+from typing import Callable
+import numpy as np
+
+
+def to_ndarray(images: Image, normalize=True):
+    images = [np.array(image) for image in images]
+    for i, image in enumerate(images):
+        if normalize and image.max() > 2:
+            images[i] = image / 255
+    images = np.stack(images, axis=0)
+    # N H W C -> N C H W
+    images = images.transpose((0, 3, 1, 2)).astype('float32')
+    print(images.shape)
+    return images
+
+
+def ctc_greedy_decode(logits, blank_id, vocab=None):
+    preds = logits.argmax(axis=-1)
+    outputs = []
+    for pred in preds:
+        output = [c for c, _ in groupby(pred) if c != blank_id]
+        outputs.append(output)
+    if vocab is not None:
+        outputs = [
+            ''.join([vocab[i] for i in output])
+            for output in outputs
+        ]
+    return outputs
+
+
+__warn_text = """
+`vocab` is a string (expected `dict`), using `blank_id = 0`
+"""
+
+
+class CTCClassifier:
+    def __init__(self,
+                 onnx_file: str,
+                 preprocess: Callable = to_ndarray,
+                 postprocess: Callable = ctc_greedy_decode,
+                 blank_id: int = 0,
+                 vocab: Optional[dict] = None,
+                 silent: bool = False):
+
+        if isinstance(vocab, str):
+            if not silent:
+                print(__warn_text)
+            blank_id = 0
+            vocab = " " + vocab
+
+        self.model = InferenceSession(
+            onnx_file,
+            providers=get_available_providers()
+        )
+        self.input_name = self.model.get_inputs()[0].name
+        self.preprocess = preprocess
+        self.postprocess = postprocess
+        self.blank_id = blank_id
+        self.vocab = vocab
+
+    def __call__(self, images: Image):
+        if not isinstance(images, (list, tuple, set)):
+            images = (images,)
+        if self.preprocess is not None:
+            images = self.preprocess(images)
+        logits, = self.model.run(None, {self.input_name: images})
+        predictions = self.postprocess(logits, self.blank_id, self.vocab)
+        return predictions
