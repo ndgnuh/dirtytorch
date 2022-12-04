9025d81a51fbaab50b0219674708e2dbe5a5669f
Hùng Nguyễn <ndgnuh@protonmail.com>
2022-12-04 14:05:30 +0700

    Metric Monitor callback

--- dirtytorch/pl/callbacks.py
+++ dirtytorch/pl/callbacks.py
@@ -1,4 +1,6 @@
 from pytorch_lightning.callbacks import Callback
+from functools import wraps
+import torch
 
 
 class GlobalStepLRScheduler(Callback):
@@ -27,3 +29,48 @@ class GlobalStepLRScheduler(Callback):
                 lr_schedulers = [lr_schedulers]
             for lr_scheduler in lr_schedulers:
                 lr_scheduler.step()
+
+
+class MetricMonitorCallback(Callback):
+    def __init__(self,
+                 metric
+                 str,
+                 mode: str,
+                 callback: Callable,
+                 on: str, model=None):
+        super().__init__()
+        assert mode in ["min", "max"]
+        assert on.startswith("on_") and hasattr(Callback, on)
+
+        # Every hook receive trainer as first argument
+        @wraps(getattr(self, on))
+        def callback_wrapper(trainer, *args, **kwargs):
+            current_metric = trainer.logged_metrics[self.metric]
+            if self.mode == "min":
+                cond = current_metric < self.current_metric
+            elif self.mode == "max":
+                cond = current_metric > self.current_metric
+            if cond:
+                self.current_metric = current_metric
+                self.callback(self, *args, **kwargs)
+
+        setattr(self, on, callback_wrapper)
+        if mode == "min":
+            self.current_metric = torch.inf
+        elif mode == "max":
+            self.current_metric = -torch.inf
+        self.callback = callback
+        self.metric = metric
+        self.mode = mode
+        self.model = model
+        self.on = on
+
+    def __repr__(self):
+        name = self.__class__.__name__
+        args = [
+            f"metric={self.metric}",
+            f"mode={self.mode}",
+            f"on={self.on}",
+        ]
+        args = ', '.join(args)
+        return f"{name}({args})"
