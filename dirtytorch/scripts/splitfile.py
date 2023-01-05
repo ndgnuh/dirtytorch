0e2abde83eafc00487b4e0ba1f2c31360482dc69
Hung Nguyen <ndgnuh@protonmail.com>
2023-01-05 15:47:01 +0700

    splitfile presets

--- dirtytorch/scripts/splitfile.py
+++ dirtytorch/scripts/splitfile.py
@@ -2,16 +2,31 @@ from argparse import ArgumentParser
 from os import path
 from random import shuffle
 
+presets = {
+    "911": ("train,val,test", "0.81,0.09,0.1"),
+    "822": ("train,val,test", "0.64,0.16,0.2"),
+    "55": ("train,test", "0.5,0.5"),
+}
+
 
 def main():
     parser = ArgumentParser()
 
     parser.add_argument("--names", default="train,test,val")
-    parser.add_argument("--ratio", default="6,2,2")
+    parser.add_argument("--ratio", default="0.64,0.2,0.16")
+    parser.add_argument("--preset",
+                        dest="preset",
+                        choices=["911", "822", "55"],
+                        default=None)
     parser.add_argument("input")
 
     # Process args
     args = parser.parse_args()
+
+    # Presets if specified
+    if args.preset is not None:
+        args.names, args.ratio = presets[args.preset]
+
     args.names = args.names.split(",")
     args.ratio = [float(r) for r in args.ratio.split(",")]
     args.ratio = [r / sum(args.ratio) for r in args.ratio]
